e116d1728804cb03321851a6283967e559319446
Danillo <danilloramosbezerra@gmail.com>
2018-12-05 22:56:35 -0200

    Login com Firebase

--- app/src/main/java/br/com/danillorcb/calculaflex/FormActivity.kt
+++ app/src/main/java/br/com/danillorcb/calculaflex/FormActivity.kt
@@ -1,12 +1,32 @@
 package br.com.danillorcb.calculaflex
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.danillorcb.calculaflex.extensions.getDouble
+import br.com.danillorcb.calculaflex.watchers.DecimalTextWatcher
+import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+        etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
+        etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
+
+        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage, 1))
+        etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage, 1))
+
+        btCalculate.setOnClickListener {
+            val proximaTela = Intent(this, ResultActivity::class.java)
+            proximaTela.putExtra("GAS_PRICE", etGasPrice.getDouble())
+            proximaTela.putExtra("ETHANOL_PRICE", etEthanolPrice.getDouble())
+            proximaTela.putExtra("GAS_AVERAGE", etGasAverage.getDouble())
+            proximaTela.putExtra("ETHANOL_AVERAGE", etEthanolAverage.getDouble())
+
+            startActivity(proximaTela)
+        }
     }
 }
--- app/src/main/java/br/com/danillorcb/calculaflex/ResultActivity.kt
+++ app/src/main/java/br/com/danillorcb/calculaflex/ResultActivity.kt
@@ -2,11 +2,34 @@ package br.com.danillorcb.calculaflex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.danillorcb.calculaflex.extensions.format
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        calculate()
+    }
+
+    private fun calculate() {
+        val gasPrice = intent.extras.getDouble("GAS_PRICE", 0.0)
+        val ethanolPrice = intent.extras.getDouble("ETHANOL_PRICE", 0.0)
+        val gasAverage = intent.extras.getDouble("GAS_AVERAGE", 0.0)
+        val ethanolAverage = intent.extras.getDouble("ETHANOL_AVERAGE", 0.0)
+
+        val performanceMyCar = ethanolAverage / gasAverage
+        val priceOfIndice = ethanolPrice / gasPrice
+
+        if (priceOfIndice <= performanceMyCar) {
+            tvSuggestion.text = getString(R.string.ethanol)
+        } else {
+            tvSuggestion.text = getString(R.string.gasoline)
+        }
+
+        tvEthanolAverageResult.text = (ethanolPrice / ethanolAverage).format(2)
+        tvGasAverageResult.text = (gasPrice / gasAverage).format(2)
     }
 }
